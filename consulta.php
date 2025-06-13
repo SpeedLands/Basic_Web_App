c57c75630ce27837b059532fd65aeb7609e911c7
Juan De Dios Perez Lopez <146788593+SpeedLands@users.noreply.github.com>
2025-06-13 17:15:50 -0500

    Update consulta.php
    Es por que la clave de la api de google estaba espuesta y causaba que cualquiera pudiera usarla para sus propios usos.

--- consulta.php
+++ consulta.php
@@ -1,6 +1,6 @@
 <?php
 
-$apiKey = 'AIzaSyCWI88ZVXHIpuYj2OsR_Hk35jbgKh6HdFs'; 
+$apiKey = // your api key; 
 
 // Recibir datos del formulario 
 $data = json_decode(file_get_contents('php://input'), true);
